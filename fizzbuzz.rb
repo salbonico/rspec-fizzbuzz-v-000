2f5190d9aa506dc60307a1045c9f979da589dd99
Steve Albonico <salbonico@gmail.com>
2019-01-11 21:16:30 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,14 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz
+def fizzbuzz(input)
+
+
+if input % 15 == 0
+  "Fizzbuzz" 
+elsif input % 3 == 0
+  "Fizz"
+elsif input % 5 == 0
+  "Buzz"
+else return nill
 
 end
