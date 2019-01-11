01f8fa683b8c5bfb1d14f1c732b5987e725d9f9c
Steve Albonico <salbonico@gmail.com>
2019-01-11 21:17:12 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,13 +2,11 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(input)
 
-
 if input % 15 == 0
-  "Fizzbuzz" 
+  "Fizzbuzz"
 elsif input % 3 == 0
   "Fizz"
 elsif input % 5 == 0
   "Buzz"
-else return nill
 
 end
